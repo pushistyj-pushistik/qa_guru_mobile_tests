34c17b51e4f009ebadd83ed01372d1d322d4bdc6
zhanna sencenko <jannas93@mail.ru>
2022-07-04 18:08:02 +0300

    added owner and attach

--- src/test/java/drivers/BrowserstackMobileDriver.java
+++ src/test/java/drivers/BrowserstackMobileDriver.java
@@ -36,7 +36,7 @@ public class BrowserstackMobileDriver implements WebDriverProvider {
 
     public static URL getBrowserstackUrl() {
         try {
-            return new URL("http://hub.browserstack.com/wd/hub");
+            return new URL(config.baseURL());
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
